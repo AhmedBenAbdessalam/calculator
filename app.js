87ef04638452cfe20a9d6389a90ec2aba44c3b4b
Ahmed Ben Abdessalam <ahmed.ben.abdessalam93@gmail.com>
2022-02-06 13:31:27 +0100

    add basic math operations

---
+++ app.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return b != 0 ? a / b : "don't divide by 0";
+}
